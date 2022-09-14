773f7a282f7e6cfac627ebc054df10c7a67f1853
jjuarez5 <julio.juarez5@outlook.com>
2022-09-13 21:37:12 -0500

    use user secrets

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -2,12 +2,17 @@ using CommandAPI.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Npgsql;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
+// npgBuilder to use the user secrets
+var npgBuilder = new NpgsqlConnectionStringBuilder(builder.Configuration.GetConnectionString("PostgreSqlConnection"));
+npgBuilder.Username = builder.Configuration["UserID"];
+npgBuilder.Password = builder.Configuration["Password"];
 // dotnet 6 requires this to be added since we 
 // bootstrap from the ground up
-builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(npgBuilder.ConnectionString));
 builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
 builder.Services.AddControllers();
 var app = builder.Build();
